9c28bf38162895467c90aabe0c84483aeaf3c0df
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 23:02:12 +0700

    mm

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,6 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
-import bcrypt from "bcryptjs";
+import bcrypt from bcryptjs;
 
 const User = sequelize.define("User", {
   id: {
