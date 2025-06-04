61b9f9f68a96646b407a97db8edfa21cf1f2285b
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 16:02:41 +0700

    mm

--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -1,5 +1,5 @@
 console.log("main.js loaded");
-const apiBase = "http://localhost:3000/api"; // atau sesuaikan dengan Cloud Run kamu
+
 import { apiBase } from "./config.js";
 
 // Debug: pastikan apiBase tersedia
