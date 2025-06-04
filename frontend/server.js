92c724397eb7de51eb35b5e1d00cfb27294b71fe
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 13:39:01 +0700

    mm

--- frontend/server.js
+++ frontend/server.js
@@ -1,16 +1,10 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
-// Path konfigurasi
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Serve static files
-app.use(express.static(path.join(__dirname, "frontend"))); // ganti sesuai folder kamu
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
