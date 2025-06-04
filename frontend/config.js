1c57a0ace3e090f68ad165871867c76100fcdc8f
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 12:52:35 +0700

    mm

--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,4 @@
 
-export const apiBase = "http://localhost:3000/api"; // atau URL backend Cloud Run
+export const apiBase = "https://be-sembako-981814770172.us-central1.run.app/api"; // atau URL backend Cloud Run
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-const apiBase = isLocalhost
-  ? 'http://localhost:8080/api'
-  : 'https://your-cloud-run-url.a.run.app/api';
+
