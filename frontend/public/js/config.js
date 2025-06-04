563799d35e106bbe70fee3c8e757af7426d4836c
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 16:11:23 +0700

    mm

--- frontend/public/js/config.js
+++ frontend/public/js/config.js
@@ -1,7 +1,7 @@
 
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
-export const apiBase = window.location.hostname.includes('localhost')
+export const API_URL = isLocalhost 
   ? "http://localhost:3000/api" 
   : "https://be-sembako-981814770172.us-central1.run.app/api";
 
