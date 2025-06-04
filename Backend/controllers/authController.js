dc9c4e6c2e08c13b9194c5b227dd4e4a0164da42
Noerralfii <nuralfisyahri82@gmail.com>
2025-06-04 23:03:53 +0700

    mm

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,19 +1,9 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from bcryptjs;
 import { User } from '../models/index.js';
 
 export const register = async (req, res) => {
   try {
-    const { password, ...rest } = req.body;
-    
-    // Hash password sebelum disimpan
-    const hashedPassword = await bcrypt.hash(password, 10);
-    
-    const user = await User.create({ 
-      ...rest, 
-      password: hashedPassword 
-    });
-    
+    const user = await User.create(req.body);
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
     res.status(201).json({ token });
   } catch (err) {
@@ -23,25 +13,12 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-  
-  try {
-    const user = await User.findOne({ where: { email } });
-    
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    
-    if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-    
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+  const user = await User.findOne({ where: { email } });
+  if (!user || !(await user.comparePassword(password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
   }
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+  res.json({ token });
 };
 
 export const getProfile = async (req, res) => {
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,79 +1,19 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/database.js";
-import bcrypt from bcryptjs;
+import bcrypt from "bcryptjs";
 
 const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  name: { 
-    type: DataTypes.STRING, 
-    allowNull: false,
-    validate: {
-      notEmpty: {
-        msg: "Nama tidak boleh kosong"
-      },
-      len: {
-        args: [3, 50],
-        msg: "Nama harus antara 3 hingga 50 karakter"
-      }
-    }
-  },
-  email: { 
-    type: DataTypes.STRING, 
-    allowNull: false, 
-    unique: {
-      msg: "Email sudah terdaftar"
-    },
-    validate: {
-      isEmail: {
-        msg: "Format email tidak valid"
-      },
-      notEmpty: {
-        msg: "Email tidak boleh kosong"
-      }
-    }
-  },
-  password: { 
-    type: DataTypes.STRING, 
-    allowNull: false,
-    validate: {
-      notEmpty: {
-        msg: "Password tidak boleh kosong"
-      },
-      len: {
-        args: [6, 100],
-        msg: "Password minimal 6 karakter"
-      }
-    }
-  },
-  role: {
-    type: DataTypes.ENUM('user', 'admin'),
-    defaultValue: 'user'
-  }
-}, {
-  timestamps: true,
-  hooks: {
-    beforeUpdate: async (user) => {
-      if (user.changed('password')) {
-        user.password = await bcrypt.hash(user.password, 10);
-      }
-    }
-  }
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
 });
 
-// Hook untuk hash password sebelum create
 User.beforeCreate(async (user) => {
-  if (user.password) {
-    user.password = await bcrypt.hash(user.password, 10);
-  }
+  user.password = await bcrypt.hash(user.password, 10);
 });
 
-// Method untuk membandingkan password
-User.prototype.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+User.prototype.comparePassword = function (password) {
+  return bcrypt.compare(password, this.password);
 };
 
-export default User;
+export default User;
